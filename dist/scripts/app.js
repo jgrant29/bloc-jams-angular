9b55f590119099058aad385aea46ac12acbfe26a
Justin Grant <jmgrant702@gmail.com>
2016-02-29 16:26:52 -0700

    assignment 3 routing and states angular

---
+++ dist/scripts/app.js
@@ -0,0 +1,29 @@
+(function() {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true, 
+        requireBase: false
+      });
+
+    $stateProvider
+    .state('landing', {
+      url: '/',
+      templateUrl: '/templates/landing.html'
+    })
+    .state('album', {
+      url: '/album',
+      templateUrl: '/tempates/album.html'
+    })
+    .state('collection', {
+      url: '/collection',
+      templateUrl: '/tempates/collection.html'
+    });
+  }
+
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+
+})();
+
