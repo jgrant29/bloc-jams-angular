08a9d255ce814f89da8817be8b83f92426870cae
Justin Grant <jmgrant702@gmail.com>
2016-03-21 12:16:09 -0600

    checkpoint 7 services part 2 comoplete

---
+++ dist/scripts/services/SongPlayer.js
@@ -0,0 +1,45 @@
+(function () {
+  function SongPlayer() {
+    var SongPlayer = {};
+
+    var currentSong = null;
+    var currentBuzzObject = null;
+
+    SongPlayer.play = function(song) {
+      if (currentSong !== song) {
+      setSong(song);
+      currentBuzzObject.play();
+      song.playing = true;
+      }
+    };
+
+    SongPlayer.pause = function(song) {
+      currentBuzzObject.pause();
+      song.playing = false;
+    }
+
+    var setSong = function(song) {
+      if (currentBuzzObject) {
+          currentBuzzObject.stop();
+          currentSong.playing = null;
+        } else if (currentSong === song) {
+          if (currentBuzzObject.isPaused()) {
+            currentBuzzObject.play();
+          }
+        };
+      
+      currentBuzzObject = new buzz.sound(song.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+      });
+
+      currentSong = song;
+    }
+
+    return SongPlayer;
+  }
+
+  angular
+      .module('blocJams')
+      .factory('SongPlayer', SongPlayer)
+})();
